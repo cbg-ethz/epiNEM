0e925038db93625682893cc16a5beec768a36b0a
MartinFXP <martin.pirkl@bsse.ethz.ch>
2016-10-18 10:58:35 +0200

    vignette bugfix

---
+++ vignettes/epiNEM.R
@@ -0,0 +1,6 @@
+## ----setup, include=FALSE, cache=FALSE--------------------------------------------------
+library(knitr)
+# set global chunk options
+opts_chunk$set(fig.path='figure/', fig.align='center', fig.show='hold')
+options(formatR.arrow=TRUE,width=90)
+
